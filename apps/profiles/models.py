a18d67a754915844c7ce75f4501d00a06d2cefdf
alkenes <belloabdulhakeemolamide@gmail.com>
2023-06-29 14:17:55 +0100

    chore: update profile apps

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -6,6 +6,10 @@ from django.utils.translation import gettext_lazy as _
 
 from django_countries.fields import CountryField
 
+from apps.profiles.utils import generate_unique_id
+
+from .constants import BUSINESS_TYPE, CURRENCY_CHOICE, INDUSTRY_TYPE
+
 
 User = get_user_model()
 
@@ -17,14 +21,30 @@ class UserProfile(models.Model):
     organization_name = models.CharField(
         max_length=100, verbose_name=_("Organization Name")
     )
-    country = CountryField()
+    location = CountryField()
     state = models.CharField(
         max_length=100, verbose_name=_("State/Province"), blank=True
     )
     city = models.CharField(max_length=100, verbose_name=_("City"), blank=True)
-
+    address = models.CharField(
+        max_length=100, blank=True, verbose_name=_("Business Address")
+    )
     phone_number = models.CharField(max_length=100)
     website = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return f"{self.user}"
+
+
+class OrganizationProfile(models.Model):
+    org_id = models.CharField(
+        primary_key=True, max_length=10, default=generate_unique_id, editable=False
+    )
+    organization = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    industry = models.CharField(max_length=100, choices=INDUSTRY_TYPE)
+    business = models.CharField(max_length=100, choices=BUSINESS_TYPE)
+    logo = models.ImageField(upload_to="")
+    base_currency = models.CharField(max_length=30, choices=CURRENCY_CHOICE, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.organization}"
---
+++ apps/profiles/utils.py
@@ -0,0 +1,12 @@
+import random
+import string
+
+from . import models
+
+
+def generate_unique_id():
+    length = 8  # Length of the generated ID excluding the starting '8'
+    while True:
+        generated_id = "80" + "".join(random.choices("0123456789", k=length))
+        if not models.OrganizationProfile.objects.filter(pk=generated_id).exists():
+            return generated_id
